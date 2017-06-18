f52a5a6d2f6288640712c8e49de5211df416ed4f
sametaylak <sametaylak29@gmail.com>
2017-06-18 11:20:50 +0300

    Gallery Intent Added

--- app/src/main/java/com/gelecegiyazankadinlar/blog/PostActivity.java
+++ app/src/main/java/com/gelecegiyazankadinlar/blog/PostActivity.java
@@ -1,10 +1,13 @@
 package com.gelecegiyazankadinlar.blog;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,8 +15,11 @@ import butterknife.OnClick;
 
 public class PostActivity extends AppCompatActivity {
 
+    private static final int GALLERY_REQUEST = 1;
+
     @BindView(R.id.post_title) EditText titleEditText;
     @BindView(R.id.post_description) EditText descriptionEditText;
+    @BindView(R.id.post_image) ImageButton postImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +31,23 @@ public class PostActivity extends AppCompatActivity {
 
     @OnClick(R.id.submit_post)
     public void submitPost() {
-        String baslik = titleEditText.getText().toString();
-        String aciklama = descriptionEditText.getText().toString();
 
-        Log.d("POST", "Başlık: " + baslik + "\nAçıklama: " + aciklama);
+    }
+
+    @OnClick(R.id.post_image)
+    public void selectPostImage() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            postImageButton.setImageURI(imageUri);
+        }
     }
 }
